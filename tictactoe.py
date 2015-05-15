cb93109f9fe1b3a721dc72f6e7150a8232c2daaf
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-05-15 18:04:17 -0500

    made empty board

---
+++ tictactoe.py
@@ -0,0 +1,22 @@
+def print_welcome_message():
+    print "Welcome to the tictactoe player!"
+
+def display_board():
+    print '---------------------'
+    print '|     |      |      |'
+    print '|     |      |      |'
+    print '---------------------'
+    print '|     |      |      |'
+    print '|     |      |      |'
+    print '---------------------'
+    print '|     |      |      |'
+    print '|     |      |      |'
+    print '---------------------'
+
+
+def main():
+    print_welcome_message()
+    display_board()
+
+if  __name__ == "__main__":
+    main()
