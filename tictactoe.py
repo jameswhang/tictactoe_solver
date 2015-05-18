1ed98ba4b39d9b4fd258451234352a3c86a9a45d
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-05-17 22:33:58 -0500

    added code to update the board depending on user input

--- tictactoe.py
+++ tictactoe.py
@@ -1,22 +1,34 @@
 def print_welcome_message():
     print "Welcome to the tictactoe player!"
 
-def display_board():
+board_array = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+def display_board(board_array):
     print '---------------------'
-    print '|     |      |      |'
-    print '|     |      |      |'
-    print '---------------------'
-    print '|     |      |      |'
+    print '| ' + board_array[0] + '   |  ' + board_array[1] + '   |  ' + board_array[2] + '   |'
     print '|     |      |      |'
     print '---------------------'
+    print '| ' + board_array[3] + '   |  ' + board_array[4] + '   |  ' + board_array[5] + '   |'
     print '|     |      |      |'
+    print '---------------------'
+    print '| ' + board_array[6] + '   |  ' + board_array[7] + '   |  ' + board_array[8] + '   |'
     print '|     |      |      |'
     print '---------------------'
 
+def get_user_input():
+    x = input('Type the x-coordinate of where you want to put your check (0 - 2): ')
+    y = input('Type the y-coordinate of where you want to put your check (0 - 2): ')
+    return [x, y]
+
+def put_check(boardarray, userInput):
+    boardarray[3*userInput[0]+userInput[1]] = 'O' 
+    display_board(boardarray)
 
 def main():
     print_welcome_message()
-    display_board()
+    display_board(board_array)
+    userInput = get_user_input()
+    put_check(board_array, userInput)
 
 if  __name__ == "__main__":
     main()
